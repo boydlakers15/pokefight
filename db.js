403317db190987745fe9483a42ffff3c2503433c
boydlakers15 <jboyd115@icloud.com>
2023-05-02 16:49:13 +0200

    Removed yarn.lock file

--- db.js
+++ db.js
@@ -11,4 +11,7 @@ mongoose.connect(DB_URI, {
 }).then(() => console.log('MongoDB connected successfully'))
   .catch(error => console.log(error));
 
+
+
+
   
--- leaderboardRouter.js
+++ leaderboardRouter.js
@@ -1,16 +1,42 @@
+// const express = require('express');
+// const Student = require('./modules/game');
+
+// const leaderboardRouter = express.Router();
+
+// leaderboardRouter.get('/comp/leaderboard', async (req, res) => {
+//   try {
+//     const games = await Student.find().sort('-createdAt').limit(10);
+//     res.status(200).json(games);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Error retrieving games' });
+//   }
+// });
+
+// module.exports = leaderboardRouter;
 const express = require('express');
-const Student = require('./modules/game');
+const router = express.Router();
+const pool = require('./db');
 
-const leaderboardRouter = express.Router();
-
-leaderboardRouter.get('/comp/leaderboard', async (req, res) => {
+router.get('/leaderboard', async (req, res) => {
   try {
-    const games = await Student.find().sort('-createdAt').limit(10);
-    res.status(200).json(games);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Error retrieving games' });
+    const { rows } = await pool.query('SELECT * FROM leaderboard ORDER BY score DESC');
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error retrieving leaderboard from database');
   }
 });
 
-module.exports = leaderboardRouter;
+router.post('/leaderboard', async (req, res) => {
+  const { name, score } = req.body;
+  try {
+    await pool.query('INSERT INTO leaderboard (name, score) VALUES ($1, $2)', [name, score]);
+    res.send('Score added to leaderboard');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error adding score to leaderboard');
+  }
+});
+
+module.exports = router;
