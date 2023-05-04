4bc6957dbaf402fc4e97c39c681cdfe8f5b60dd4
boydlakers15 <jboyd115@icloud.com>
2023-05-04 11:55:09 +0200

    new

--- saveRouter.js
+++ saveRouter.js
@@ -4,11 +4,12 @@ const router = express.Router();
 
 // Handle POST requests to the '/save' endpoint
 router.post('/save', (req, res) => {
+  const { playerPokemon, opponentPokemon, winner, date } = req.body;
   const newGame = new Game({
-    playerPokemon: req.body.playerPokemon,
-    opponentPokemon: req.body.opponentPokemon,
-    winner: req.body.winner,
-    turns: req.body.turns
+    playerPokemon,
+    opponentPokemon,
+    winner,
+    date,
   });
 
   newGame.save()
@@ -18,7 +19,7 @@ router.post('/save', (req, res) => {
     .catch((error) => {
       console.error(error);
       res.status(500).send('Error saving game to database');
-    })
+    });
 });
 
 module.exports = router;
