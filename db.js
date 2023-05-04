7426fa127b74a9f3d43a58ceee369db3376cc8ef
boydlakers15 <jboyd115@icloud.com>
2023-05-04 10:59:16 +0200

    new

--- db.js
+++ db.js
@@ -8,6 +8,7 @@ const DB_URI = 'mongodb+srv://jboyd:OibvL7Uxyzpeqq1J@cluster0.klbynli.mongodb.ne
 mongoose.connect(DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
 }).then(() => console.log('MongoDB connected successfully'))
   .catch(error => console.log(error));
 
