5d6d15f1d0505a3542900fb8bb283f2081744732
boydlakers15 <jboyd115@icloud.com>
2023-05-04 13:38:17 +0200

    testing

--- router/router.js
+++ router/router.js
@@ -4,6 +4,27 @@ const pokemonController = require('../controllers/pokemonController');
 
 app.get('/pokemon', pokemonController.getAllPokemon);
 
+router.post('/game/save', async (req, res) => {
+    try {
+      const gameData = req.body;
+      const newGame = new Game(gameData);
+      await newGame.save();
+      res.status(200).send('Game saved');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error saving game');
+    }
+  });
 
+  router.get('/game/leaderboard', async (req, res) => {
+    try {
+      const games = await Game.find({});
+      res.json(games);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error retrieving games');
+    }
+  });
+  
 
 module.exports = router;
