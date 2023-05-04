5d657028cb1b8d49cc25a1baa155e2e574ba6e0b
boydlakers15 <jboyd115@icloud.com>
2023-05-04 15:04:07 +0200

    new

--- db.js
+++ db.js
@@ -1,16 +1,8 @@
-
 const mongoose = require('mongoose');
 
 
-// const DB_URI = 'mongodb+srv://jboyd:OibvL7Uxyzpeqq1J@cluster0.klbynli.mongodb.net/test';
-// mongoose.connect(DB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverSelectionTimeoutMS: 5000,
-// }).then(() => console.log('MongoDB connected successfully'))
-//   .catch(error => console.log(error));
-
-mongoose.connect(process.env.MONGODB_URI, {
+const DB_URI = 'mongodb+srv://jboyd:OibvL7Uxyzpeqq1J@cluster0.klbynli.mongodb.net/test';
+mongoose.connect(DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverSelectionTimeoutMS: 5000,
