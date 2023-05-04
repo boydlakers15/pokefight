78c45935ae903a87c957cba01ebeff240910e623
boydlakers15 <jboyd115@icloud.com>
2023-05-04 08:10:50 +0200

    Add node-fetch as dependency

--- modules/game.js
+++ modules/game.js
@@ -1,15 +1,15 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const gameSchema = new mongoose.Schema({
-//   playerPokemon: { type: String, required: true },
-//   opponentPokemon: { type: String, required: true },
-//   winner: { type: String, required: true },
-//   createdAt: { type: Date, default: Date.now }
-// });
+const gameSchema = new mongoose.Schema({
+  playerPokemon: { type: String, required: true },
+  opponentPokemon: { type: String, required: true },
+  winner: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
 
-// const Game = mongoose.model('Game', gameSchema);
+const Game = mongoose.model('Game', gameSchema);
 
-// module.exports = Game;
+module.exports = Game;
 
 
 
