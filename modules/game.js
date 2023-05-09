1ace4cebc83ab9b98caa625c31deb859ece7ee66
boydlakers15 <jboyd115@icloud.com>
2023-05-09 15:08:57 +0200

    testing

--- modules/game.js
+++ modules/game.js
@@ -9,7 +9,33 @@ const gameSchema = new mongoose.Schema({
 
 const Game = mongoose.model('Game', gameSchema);
 
-module.exports = Game;
+const userSchema = new mongoose.Schema({
+    _id: { type: mongoose.Types.ObjectId, auto: true },
+    username: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 20,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        maxLength: 1024,
+    }
+});
+
+userSchema.set('toJSON', {
+  transform: function (doc, ret, options) {
+    ret.password = String(ret.password);
+    return ret;
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { Game, User };
+
 
 
 
