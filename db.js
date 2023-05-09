893b23be596454d831cc36a8c91afe72adf9dff2
boydlakers15 <jboyd115@icloud.com>
2023-05-09 13:14:15 +0200

    testing

--- db.js
+++ db.js
@@ -11,4 +11,11 @@ mongoose.connect(DB_URI, {
 }).then(() => console.log('MongoDB connected successfully'))
   .catch(error => console.log(error));
 
+  const MONGODBURI = 'mongodb+srv://jboyd:OibvL7Uxyzpeqq1J@cluster0.klbynli.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(MONGODBURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected successfully'))
+  .catch(error => console.log(error));
+
   
