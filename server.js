3abc4e2a5fc0f0d09534e1ed406dd85068999fb5
boydlakers15 <jboyd115@icloud.com>
2023-05-09 17:29:30 +0200

    new

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/pokemon', getAllPokemon);
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173/'
+  origin: '*'
 }));
 
 // Add session configuration
