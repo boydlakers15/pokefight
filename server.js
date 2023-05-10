52b3bcb16c143db57c647648b97f7fb0e1f0d6f9
boydlakers15 <jboyd115@icloud.com>
2023-05-10 20:47:18 +0200

    checking to see if it saves local

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const sess = {
 // Use session middleware
 app.use(session(sess));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://645bdca8dd0a552f85001b95--pokemon-grp-3.netlify.app");
+  res.header("Access-Control-Allow-Origin", "https://645be67a7405be0283935ab8--pokemon-grp-3.netlify.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
