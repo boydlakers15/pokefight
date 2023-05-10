089a569fde1d38f0ec50c1f73bdb90506bffb09a
boydlakers15 <jboyd115@icloud.com>
2023-05-10 19:19:29 +0200

    testing

--- server.js
+++ server.js
@@ -43,6 +43,84 @@ const secret = process.env.JWT_SECRET;
 const PORT = process.env.PORT;
 // mongoose.connect(process.env.MONGODBURI);
 
+// GET /users ⇒ return all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to get users' });
+  }
+});
+
+// GET /users/:id ⇒ return a single user
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+
+// POST /users ⇒ create a new user
+app.post('/users', async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ error: 'Missing username or password' });
+  }
+  try {
+    const newUser = new User({ username: req.body.username, password: req.body.password });
+    const user = await newUser.save();
+    const token = jwt.sign({ id: user._id }, secret);
+    res.cookie('token', token, { httpOnly: true });
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to create user' });
+  }
+});
+
+// PUT /users/:id ⇒ update a specific user
+app.put('/users/:id', async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ error: 'Missing username or password' });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      username: req.body.username,
+      password: req.body.password,
+    });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+
+// DELETE /users/:id ⇒ delete a specific user
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.continue('/login')
+      .status(404)
+      .json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+
 // POST /login ⇒ login and return
 app.post('/login', async (req, res) => {
     try {
