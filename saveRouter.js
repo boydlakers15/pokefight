1d761e81fd8b7e6685683b205908b35b6b11c8e3
boydlakers15 <jboyd115@icloud.com>
2023-05-10 20:35:14 +0200

    reverted back

--- saveRouter.js
+++ saveRouter.js
@@ -3,23 +3,23 @@ const Game = require('./modules/game');
 const router = express.Router();
 
 // Handle POST requests to the '/save' endpoint
-// router.post('/save', async (req, res) => {
-//   const { playerPokemon, opponentPokemon, winner, date } = req.body;
+router.post('/save', async (req, res) => {
+  const { playerPokemon, opponentPokemon, winner, date } = req.body;
 
-//   try {
-//     const game = new Game({
-//       playerPokemon,
-//       opponentPokemon,
-//       winner,
-//       createdAt: date,
-//     });
-//     const savedGame = await game.save();
-//     res.status(201).json(savedGame);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: 'Failed to save game' });
-//   }
-// });
+  try {
+    const game = new Game({
+      playerPokemon,
+      opponentPokemon,
+      winner,
+      createdAt: date,
+    });
+    const savedGame = await game.save();
+    res.status(201).json(savedGame);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Failed to save game' });
+  }
+});
 
 router.post('/save', (req, res) => {
   const gameData = req.body;
