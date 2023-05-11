1824894ee1e1d5369900779771e85a58cffcd63c
boydlakers15 <jboyd115@icloud.com>
2023-05-11 10:03:36 +0200

    Testing new code

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,69 +1,133 @@
-const UserCollection = require('../modules/user');
+const User = require('../modules/user');
 const ErrorStatus = require('../utils/errorStatus');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const login = async (req, res, next) => {
+const getUsers = async (req, res) => {
   try {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      throw new ErrorStatus('Missing fields', 400);
-    }
-
-    const findUser = await UserCollection.findOne({ username }).select('+password');
-    if (!findUser) {
-      throw new ErrorStatus('User not found!', 404);
-    }
-
-    const compare = await bcrypt.compare(password, findUser.password);
-    if (!compare) {
-      throw new ErrorStatus('Password does not match!', 401);
-    }
-
-    const token = jwt.sign({ _id: findUser._id }, process.env.JWT_SECRET);
-
-    return res
-      .cookie('token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 })
-      .sendStatus(200);
+    const users = await User.find({});
+    res.status(200).json(users);
   } catch (error) {
-    next(error);
+    console.error(error);
+    res.status(400).json({ error: 'Failed to get users' });
   }
 };
 
-
-const createUser = async (req, res, next) => {
+const getUserById = async (req, res) => {
   try {
-    const { firstName, lastName, email, password } = req.body;
-    if (!firstName || !lastName || !email || !password)
-      throw new ErrorStatus('Missing fields', 400);
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
+  }
+};
 
-    const hash = await bcrypt.hash(password, 10);
+const createUser = async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ error: 'Missing username or password' });
+  }
+  try {
+    const newUser = new User({ username: req.body.username, password: req.body.password });
+    const user = await newUser.save();
+    const token = jwt.sign({ id: user._id }, secret);
+    res.cookie('token', token, { httpOnly: true });
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to create user' });
+  }
+};
 
-    const { _id } = await UserCollection.create({
-      username,
-      firstName,
-      lastName,
-      email,
-      password: hash,
+const updateUser = async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ error: 'Missing username or password' });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      username: req.body.username,
+      password: req.body.password,
     });
-
-    const token = jwt.sign({ _id }, process.env.JWT_SECRET);
-
-    res
-      .cookie('token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 })
-      .sendStatus(201);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
   } catch (error) {
-    next(error);
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
   }
 };
 
-const getOneUser = async (req, res, next) => {
+const deleteUser = async (req, res) => {
   try {
-    const findUser = await UserCollection.findById(req.userId);
-    res.status(200).json(findUser);
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
   } catch (error) {
-    next(error);
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
   }
 };
 
-module.exports = { login, getOneUser, createUser };
+const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+    if (user.password !== req.body.password) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+    const token = jwt.sign({ id: user._id }, secret);
+    req.session.user = user; // Store the user in the session
+    res.cookie('token', token, { httpOnly: true });
+    res.status(200).json({ message: 'Logged in successfully', token });
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ error: 'Invalid username or password' });
+  }
+};
+
+const signup = async (req, res) => {
+  const { firstName, lastName, email, username, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).send('Username already exists');
+    }
+    const user = await User.create({ firstName, lastName, email, username, password });
+    if (!user) {
+      return res.status(500).send('Failed to create user');
+    }
+    const token = jwt.sign({ id: user._id }, secret);
+    req.session.user = user; // Store the user in the session
+    res.cookie('token', token, { httpOnly: true });
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  }
+};
+
+const logout = (req, res) => {
+  req.session.destroy(); // Destroy the session
+  res.clearCookie('token');
+  res.status(200).json({ message: 'Logged out successfully' });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  login,
+  signup,
+  logout,
+};
+
--- router/userRoutes.js
+++ router/userRoutes.js
@@ -1,13 +1,38 @@
-const userRouter = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const {
-  login,
-  getOneUser,
+  getUsers,
+  getUserById,
   createUser,
+  updateUser,
+  deleteUser,
+  login,
+  signup,
+  logout,
 } = require('../controllers/userControllers');
-const checkToken = require('../middlewares/checkToken');
 
-userRouter.route('/login').post(login);
-userRouter.route('/signup').post(createUser);
-userRouter.route('/me').get(checkToken, getOneUser);
+// GET /users - Get all users
+router.get('/users', getUsers);
 
-module.exports = userRouter;
+// GET /users/:id - Get a single user
+router.get('/users/:id', getUserById);
+
+// POST /users - Create a new user
+router.post('/users', createUser);
+
+// PUT /users/:id - Update a user
+router.put('/users/:id', updateUser);
+
+// DELETE /users/:id - Delete a user
+router.delete('/users/:id', deleteUser);
+
+// POST /login - User login
+router.post('/login', login);
+
+// POST /signup - User signup
+router.post('/signup', signup);
+
+// GET /logout - User logout
+router.get('/logout', logout);
+
+module.exports = router;
