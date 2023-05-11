5ec1da3d16bca0dcd25f326bf6b7f546ceefa02c
boydlakers15 <jboyd115@icloud.com>
2023-05-11 11:29:58 +0200

    testing

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,11 +1,84 @@
 const express = require('express');
-const router = express.Router();
-const userController = require('./userController');
+const jwt = require('jsonwebtoken');
+const User = require('./modules/user');
 
-router.get('/', userController.getUsers);
-router.get('/:id', userController.getUserById);
-router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+const router = express.Router();
+const secret = process.env.JWT_SECRET;
+
+// GET /users ⇒ return all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to get users' });
+  }
+});
+
+// GET /users/:id ⇒ return a single user
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+
+// POST /users ⇒ create a new user
+router.post('/', async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ error: 'Missing username or password' });
+  }
+  try {
+    const newUser = new User({ username: req.body.username, password: req.body.password });
+    const user = await newUser.save();
+    const token = jwt.sign({ id: user._id }, secret);
+    res.cookie('token', token, { httpOnly: true });
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to create user' });
+  }
+});
+
+// PUT /users/:id ⇒ update a specific user
+router.put('/:id', async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ error: 'Missing username or password' });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      username: req.body.username,
+      password: req.body.password,
+    });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+
+// DELETE /users/:id ⇒ delete a specific user
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.continue('/login').status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
+  }
+});
 
 module.exports = router;
