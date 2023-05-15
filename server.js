e1d9285d4b605701a3d034af65650898ff70e7db
boydlakers15 <jboyd115@icloud.com>
2023-05-15 12:07:32 +0200

    testing

--- server.js
+++ server.js
@@ -2,63 +2,168 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-
 const leaderboardRouter = require('./leaderboardRouter');
 const saveRouter = require('./saveRouter');
-const userRouter = require('./router/usersRouter');
+const bodyParser = require('body-parser');
 const { getAllPokemon } = require('./controllers/pokemonController');
-const User = require('./modules/user');
 require('./db');
-
 // Add session middleware
 const session = require('express-session');
-
 const app = express();
-
 // Middleware
-app.use(bodyParser.json());
 app.use(express.json());
-
 const corsOptions = {
-  origin: 'https://645c977dfee4d567a8bbf524--pokemon-grp-3.netlify.app'
+  origin: 'https://646202de35d6d50ab5f8da67--pokemon-grp-3.netlify.app'
 };
 
-// Routes
+app.use(cors(corsOptions));
+const User  = require('./modules/user');
+
+const secret = process.env.JWT_SECRET;
 app.use('/', leaderboardRouter);
 app.use('/', saveRouter);
-app.use('/users', userRouter);
+app.use(bodyParser.json());
 app.get('/pokemon', getAllPokemon);
-app.use(cors(corsOptions));
-
-// Database connections
-const secret = process.env.JWT_SECRET;
-const PORT = process.env.PORT;
-mongoose.connect(process.env.MONGODBURI);
-
 // Add session configuration
 const sess = {
   secret: 'keyboard cat',
   cookie: {}
 };
-
 // Use session middleware
 app.use(session(sess));
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://646202de35d6d50ab5f8da67--pokemon-grp-3.netlify.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
+const PORT = process.env.PORT;
+// mongoose.connect(process.env.MONGODBURI);
+// GET /users ⇒ return all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to get users' });
+  }
+});
+// GET /users/:id ⇒ return a single user
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+// POST /users ⇒ create a new user
+app.post('/users', async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ error: 'Missing username or password' });
+  }
+  try {
+    const newUser = new User({ username: req.body.username, password: req.body.password });
+    const user = await newUser.save();
+    const token = jwt.sign({ id: user._id }, secret);
+    res.cookie('token', token, { httpOnly: true });
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to create user' });
+  }
+});
+// PUT /users/:id ⇒ update a specific user
+app.put('/users/:id', async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ error: 'Missing username or password' });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      username: req.body.username,
+      password: req.body.password,
+    });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+// DELETE /users/:id ⇒ delete a specific user
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.continue('/login')
+      .status(404)
+      .json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+// POST /login ⇒ login and return
+app.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+        if (user.password !== req.body.password) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+        const token = jwt.sign({ id: user._id }, secret);
+        req.session.user = user; // Store the user in the session
+        res.cookie('token', token, { httpOnly: true });
+        res.status(200).json({ message: 'Logged in successfully', token });
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({ error: 'Invalid username or password' });
+    }
+});
+// POST /signup ⇒ create a new user
+app.post('/signup', async (req, res) => {
+  const { firstName, lastName, email, username, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).send('Username already exists');
+    }
+    const user = await User.create({ firstName, lastName, email, username, password });
+    if (!user) {
+      return res.status(500).send('Failed to create user');
+    }
+    const token = jwt.sign({ id: user._id }, secret);
+    req.session.user = user; // Store the user in the session
+    res.cookie('token', token, { httpOnly: true });
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  }
+});
+// GET /logout ⇒ logout and clear JWT cookie
+app.get('/logout', (req, res) => {
+    req.session.destroy(); // Destroy the session
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logged out successfully' });
+});
 fetch('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
   .then(response => response.json())
   .then(pokemonData => {
     app.get('/pokemon', (req, res) => {
       res.json(pokemonData);
     });
-
     app.get('/pokemon/:id', (req, res) => {
       const id = parseInt(req.params.id);
       const pokemon = pokemonData.find(p => p.id === id);
@@ -68,7 +173,6 @@ fetch('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.jso
         res.json(pokemon);
       }
     });
-
     app.get('/pokemon/:id/:info', (req, res) => {
       const id = parseInt(req.params.id);
       const pokemon = pokemonData.find(p => p.id === id);
@@ -83,10 +187,9 @@ fetch('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.jso
         }
       }
     });
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+    
+    
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(error => {
     console.log(error);
